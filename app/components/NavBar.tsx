0508a0a0f635f195a679a85ab0179df09c744b12
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-08 21:24:17 -0400

    styled navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="w-full fixed top-0 left-0 z-50 backdrop-blur-md bg-white/10 border-b border-white/30">
-      <div className="max-w-7xl mx-auto px-4 py-3 flex justify-center">
-        <ul className="flex gap-6 text-white text-lg">
-          <li className="hover:underline cursor-pointer">Home</li>
-          <li className="hover:underline cursor-pointer">About</li>
-          <li className="hover:underline cursor-pointer">Contact</li>
+    <nav className="w-full h-25 fixed top-0 left-0 z-50 backdrop-blur-md bg-white/10">
+      <div className="max-w-7xl h-full mx-auto px-4 flex justify-center items-center">
+        <ul className="flex gap-8 text-white text-lg">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
       </div>
     </nav>
