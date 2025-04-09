bccf4f832753059ed058e5430028a8338d26ddae
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-09 11:01:20 -0400

    created footer for bottom nav

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <>
+    <div className="flex justify-center">
+      <div>Github</div>
+      <div>Art Collections</div>
+      <div>resume</div>
+    </div>
+    </>
+  );
+};
+
+export default footer;
