39b42d1caf0ebc49f4d030063ba0d7f8539adbd6
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-18 16:22:53 -0400

    changed items to name

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -9,28 +9,44 @@ interface Art {
 }
 
 const myData: Art[] = [
-  { id: 1, name: "Item 1", url: "/bookFear.png" },
-  { id: 2, name: "Item 2", url: "/clockFearSmaller.PNG" },
-  { id: 3, name: "Item 3", url: "/puppetFear.png" },
-  { id: 4, name: "Item 4", url: "/teckFear.png" },
-  { id: 5, name: "Item 5", url: "/hands fear.png" },
-  { id: 6, name: "Item 6", url: "/maskFear.png" },
-  { id: 7, name: "Item 7", url: "/needleFear.png" },
-  { id: 8, name: "Item 8", url: "/watchingFearSmaller.PNG" },
-  { id: 9, name: "Item 9", url: "/death fear.png" },
-  { id: 10, name: "Item 15", url: "/giordnaos vanguard 3-26-24.png" },
-  { id: 11, name: "Item 11", url: "/paragon Packageing (1).png" },
-  { id: 12, name: "Item 12", url: "/giordanos needle hey 3-21-24 (3).png" },
-  { id: 13, name: "Item 13", url: "/giordanos pharmacy 3-26-24 revamped.png" },
-  { id: 14, name: "Item 14", url: "/giordanos timberjacks 3-21-24 (1).png" },
-  { id: 15, name: "Item 10", url: "/giordanos logic knots 3-26-24 (2).png" },
-  { id: 16, name: "Item 16", url: "/extraLife.png" },
+  { id: 1, name: "Bibliophobia ", url: "/bookFear.png" },
+  { id: 2, name: "Chronomentrophobia", url: "/clockFearSmaller.PNG" },
+  { id: 3, name: "Automatonophobia", url: "/puppetFear.png" },
+  { id: 4, name: "Technology", url: "/teckFear.png" },
+  { id: 5, name: "Dysmorphia", url: "/hands fear.png" },
+  { id: 6, name: "Masklophobia", url: "/maskFear.png" },
+  { id: 7, name: "Trypanophobia", url: "/needleFear.png" },
+  { id: 8, name: "Scopophobia ", url: "/watchingFearSmaller.PNG" },
+  { id: 9, name: "thanatophobia", url: "/death fear.png" },
+  { id: 10, name: "Vanguard Pests", url: "/giordnaos vanguard 3-26-24.png" },
+  { id: 11, name: "Paragon Packaging", url: "/paragon Packageing (1).png" },
+  {
+    id: 12,
+    name: "Needle Antiques",
+    url: "/giordanos needle hey 3-21-24 (3).png",
+  },
+  {
+    id: 13,
+    name: "Alchemy Pharmacy",
+    url: "/giordanos pharmacy 3-26-24 revamped.png",
+  },
+  {
+    id: 14,
+    name: "Timberjacks Lumber",
+    url: "/giordanos timberjacks 3-21-24 (1).png",
+  },
+  {
+    id: 15,
+    name: "Logic Knots Puzzles",
+    url: "/giordanos logic knots 3-26-24 (2).png",
+  },
+  { id: 16, name: "Xtra Life Batteries", url: "/extraLife.png" },
   {
     id: 17,
-    name: "Item 17",
+    name: "Compass Flights",
     url: "/giordanos compass flights 3-26-24 (1).png",
   },
-  { id: 18, name: "Item 18", url: "/AS.png" },
+  { id: 18, name: "Alternate Solutions", url: "/AS.png" },
 ];
 
 export default function Collection() {
