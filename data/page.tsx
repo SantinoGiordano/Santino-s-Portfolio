985fe3dfb83a609270ffbd7346d871321a1c669d
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-04 21:19:24 -0400

    added data page

---
+++ data/page.tsx
@@ -0,0 +1,16 @@
+const Data = [
+  {
+    name: "ATP Exphys Website",
+    link: "/https://www.atp-exphys.com",
+  },
+  {
+    name: "Artwork 2",
+    url: "/https://github.com/SantinoGiordano/FurnitureStoreFrontend",
+  },
+  {
+    name: "Artwork 3",
+    url: "/https://elitetreeandsnowllc.com/",
+  },
+];
+
+export default Data;
