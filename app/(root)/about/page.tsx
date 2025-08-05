3a0784a441061cb11487e1be265f1a4fd4b3f64c
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-04 22:00:35 -0400

    changed the styling

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,10 +2,11 @@ import Data from "@/data/page";
 
 export default function About() {
   return (
-    <div className="bg-gradient-to-br from-black via-gray-900 to-black min-h-screen flex items-center justify-center px-4 pt-20">
+    <div className="p-10 bg-gradient-to-br from-black via-gray-900 to-black min-h-screen flex items-center justify-center px-4 pt-20">
       <div className="w-full max-w-3xl p-4 sm:p-8 bg-white/90 backdrop-blur-md shadow-2xl rounded-3xl border border-gray-200">
-        <h1 className="text-3xl sm:text-4xl font-extrabold text-gray-800 mb-6">
+        <h1 className=" text-3xl sm:text-4xl font-extrabold text-gray-800 mb-6">
           About Me
+          <hr/>
         </h1>
         <p className="text-base sm:text-lg text-gray-700 leading-relaxed tracking-wide max-w-prose">
           I am a motivated Web Developer with a double major in Computer Science
