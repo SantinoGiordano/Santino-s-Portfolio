0ba1e231d2da11ec1434526433c8a0bd90313cba
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-04 21:01:29 -0400

    added footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,12 @@
-import Link from "next/link";
+
 import React from "react";
 
 const Footer = () => {
   return (
     <>
-      <div className="bg-black p-10 text-white flex justify-center gap-10">
-        <Link
-          href="https://github.com/SantinoGiordano"
-          className="hover:text-gray-400 transition"
-        >
-          Github
-        </Link>
-        <Link href="/collection" className="hover:text-gray-400 transition">
-          Art Collections
-        </Link>
-        <Link href="./Resume.pdf" className="hover:text-gray-400 transition">
-          Resume
-        </Link>
-      </div>
+     <div className="m-8 text-center text-gray-600">
+        <p>© {new Date().getFullYear()} Santino Giordano. All rights reserved.</p>
+    </div>
     </>
   );
 };
