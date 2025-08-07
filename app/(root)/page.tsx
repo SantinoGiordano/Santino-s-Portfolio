a3a6cd5c101c7cf8aa33a5b783629c2b86e00222
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-07 11:33:52 -0400

    changed styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
-    <>
-      <div className="bg-gradient-to-br from-black via-gray-900 to-black min-h-screen flex items-center justify-center px-4">
-        <h1 className="text-white text-6xl">Santino Giordano</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-900 to-black px-4">
+      <div className="text-center">
+        <h1 className="text-white text-6xl font-bold mb-4">Santino Giordano</h1>
+        <h3 className="text-white text-2xl">Software Engineer</h3>
       </div>
-    </>
+    </div>
   );
 }
