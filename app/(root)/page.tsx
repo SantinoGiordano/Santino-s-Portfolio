43aa7f8475900d24f698e9f508637a33f0596914
SantinoGiordano <santinogiordano13@gmail.com>
2025-11-11 12:08:27 -0500

    rotating text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-900 to-black px-4">
       <div className="text-center">
-        <h1 className="text-white text-6xl font-bold mb-4">Santino Giordano</h1>
-        <h3 className="text-white text-2xl">Software Engineer</h3>
+           <h1 className="text-white text-7xl font-bold mb-4">Santino Giordano</h1>
+        <span className="text-rotate text-5xl">
+          <span className="justify-items-center text-white ">
+            <span>SOFTWARE DEVELOPER</span>
+            <span>DESIGNER</span>
+            <span>CREATOR</span>
+          </span>
+        </span>
       </div>
     </div>
   );
