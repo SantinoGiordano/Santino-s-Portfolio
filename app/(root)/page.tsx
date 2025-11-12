3033a8f7f6670d6eb59a517fdd299b1f4b6941bb
SantinoGiordano <santinogiordano13@gmail.com>
2025-11-12 13:06:11 -0500

    added innavator, and got rid of the mount clause

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,19 @@
 
-"use client";
-
-import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null; // prevents SSR mismatch
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-900 to-black px-4">
       <div className="text-center">
-        <h1 className="text-white text-7xl font-bold mb-4">Santino Giordano</h1>
-        <div className="text-5xl text-white">
-          <span className="text-rotate">
+           <h1 className="text-white text-7xl font-bold mb-4">Santino Giordano</h1>
+        <span className="text-rotate text-5xl">
+          <span className="justify-items-center text-white ">
             <span>DEVELOPER</span>
             <span>DESIGNER</span>
             <span>CREATOR</span>
+            <span>INNOVATOR</span>
             <span>LEARNER</span>
           </span>
-        </div>
+        </span>
       </div>
     </div>
   );
