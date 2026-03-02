9e84aa8c6ec089b0ff98c9ffa989db1117fe71d0
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-02 10:46:58 -0500

    reorganized the posses at which i work for the text rotate

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,12 +15,15 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-900 to-black px-4">
       <div className="text-center">
         <h1 className="text-white text-7xl font-bold mb-4">Santino Giordano</h1>
-        <span className="text-rotate text-5xl text-white">
-          <span>DEVELOPER</span>
-          <span>DESIGNER</span>
-          <span>CREATOR</span>
-          <span>INNOVATOR</span>
-          <span>LEARNER</span>
+        <span className="text-rotate text-7xl">
+          <span className="justify-items-center text-white ">
+            <span>DEVELOP</span>
+            <span>LEARN</span>
+            <span>DESIGN</span>
+            <span>DEPLOY</span>
+            <span>MAINTAIN</span>
+            <span>REPEAT</span>
+          </span>
         </span>
       </div>
     </div>
